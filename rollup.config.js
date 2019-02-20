ed6f8bb42f37b74c971b534ef78f6eaf2a4df71c
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-02-20 10:37:12 -0800

    v0.1.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import node from "rollup-plugin-node-resolve";
 
 export default {
   input: "index",
-  external: ['vega-loader'],
-  globals: {'vega-loader': 'vega'},
   plugins: [node()],
   output: {
     file: "build/dataflow-api.js",
